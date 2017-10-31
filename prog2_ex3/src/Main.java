d518b55c45336351984f14a2463161504f8ba6c6
Seiya Nakamura <e175711@ie.u-ryukyu.ac.jp>
2017-10-31 23:34:46 +0900

    Add npm start script

--- prog2_ex3/src/Main.java
+++ prog2_ex3/src/Main.java
@@ -5,5 +5,9 @@ public class Main {
 	Dice dice = new Dice();
 	dice.play();
 	System.out.println(dice.getValue());
+	for (int i = 0;i < 10; i ++) {
+	    ExDice exdice = new ExDice(5,7);
+	    System.out.println("exdice"+i+"回目: "+exdice.getValue());
+	}
     }
 }
